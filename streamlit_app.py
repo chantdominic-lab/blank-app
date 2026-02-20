62301f4c7556d262348332b4a1673cc588f0652b
Dominic Chant <chantdominic@gmail.com>
2026-02-20 22:59:30 +0100

    Update streamlit_app.py with new content

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 Snovi i Vizije")
+st.title("☁️ Snovi i Vizije")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
